8fe1256dc3a0011bb239cfd370b19e7a4c933014
Muhammad Farooq <142155448+MuhammadFarooq01@users.noreply.github.com>
2024-11-24 17:42:06 +0500

    Add: more colors

--- Web/tailwind.config.js
+++ Web/tailwind.config.js
@@ -7,16 +7,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#1E4488', // Primary Blue
-        accent: '#63CBEC',  // Accent Sky Blue
+        primary: '#295FC2', // Primary Blue
+        accent: '#275BC3',  // Accent Sky Blue
         white: '#FFFFFF',   // Neutral White
         grayLight: '#F5F5F5', // Soft Gray
+        grayMedium: '#B0B0B0', // Medium Gray (new color)
         error: '#FF6B6B',   // Error Red
         success: '#28A745', // Success Green
         warning: '#FFCC00', // Warning Yellow
-        grayDark: '#34495E', // Dark Gray
+        grayDark: '#9093a1', // Dark Gray
         divider: '#ECEFF1', // Light Gray
         black: '#000000',   // Text Black
+        lightFrost: '#F9FBFC',   // Text Black
       },
     },
   },
