1d28d6d02cda208d3a206fc2fb1c36849641ea57
Muhammad Farooq <142155448+MuhammadFarooq01@users.noreply.github.com>
2024-11-24 17:56:11 +0500

    Add cookieParser middleware

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { configDotenv } from "dotenv"
+import cookieParser from "cookie-parser"
 
 configDotenv()
 const PORT = process.env.PORT
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT:${PORT}`)
